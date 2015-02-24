cc033494bd2995557e9b34850d902f9ecb1c5f88
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2015-02-24 18:49:50 +0300

    Recreated project

---
+++ PenSamples.Web/Controllers/SecretsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PenSamples.Web.Controllers
+{
+    public class SecretsController : Controller
+    {
+        // GET: Secrets
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
