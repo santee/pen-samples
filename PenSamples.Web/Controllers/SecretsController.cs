a3a008771561ef2e10e9a569aaf4c56fcbc361b4
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2015-02-24 20:55:49 +0300

    Added some actions

--- PenSamples.Web/Controllers/SecretsController.cs
+++ PenSamples.Web/Controllers/SecretsController.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace PenSamples.Web.Controllers
+﻿namespace PenSamples.Web.Controllers
 {
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Mvc;
+
     using PenSamples.Web.Data;
     using PenSamples.Web.Models;
 
@@ -13,21 +13,48 @@ namespace PenSamples.Web.Controllers
     public class SecretsController : Controller
     {
         // GET: Secrets
-        public ActionResult List()
+        public async Task<ActionResult> List()
         {
             var username = User.Identity.Name;
 
             using (var entities = new PenEntities())
             {
-                var secrets =
-                    entities.Secrets.Select(
-                        x => new SecretModel() { Description = x.Description, Text = x.Text, User = username });
+                var secrets = await entities
+                    .Users
+                    .Where(x => x.Name == username)
+                    .SelectMany(x => x.Secrets)
+                    .Select(x => new SecretModel() { Description = x.Description, Text = x.Text, User = x.User.Name })
+                    .ToListAsync();
 
                 return View(new SecretsListModel()
                                 {
                                     User = username,
                                     Secrets = secrets
                                 });
+            }
+        }
+
+
+        // GET: Secrets/1
+        public async Task<ActionResult> Details(int secretId)
+        {
+            var username = User.Identity.Name;
+            //NOTE: We do not check user
+            using (var entities = new PenEntities())
+            {
+                var secret = await entities.Secrets.FirstOrDefaultAsync(x => x.SecretId == secretId);
+
+                if (secret == null)
+                {
+                    throw new HttpException(404, "Secret not found");
+                }
+
+                return this.View(new SecretModel
+                                     {
+                                         Description = secret.Description,
+                                         Text = secret.Text,
+                                         User = secret.User.Name
+                                     });
             }
         }
     }
