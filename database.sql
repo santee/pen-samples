564f34aa6f1874ea439dd4f1146a505f8f69218d
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2015-02-24 20:59:17 +0300

    Added database script

---
+++ database.sql
@@ -0,0 +1,40 @@
+USE [PenSamples]
+GO
+/****** Object:  Table [dbo].[Secrets]    Script Date: 2/24/2015 8:58:21 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Secrets](
+	[SecretId] [int] NOT NULL,
+	[Description] [nvarchar](max) NOT NULL,
+	[Text] [nvarchar](max) NOT NULL,
+	[UserId] [int] NOT NULL,
+ CONSTRAINT [PK_Secrets] PRIMARY KEY CLUSTERED 
+(
+	[SecretId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 2/24/2015 8:58:21 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[UserId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Status] [nvarchar](max) NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[Secrets]  WITH CHECK ADD  CONSTRAINT [FK_Secrets_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([UserId])
+GO
+ALTER TABLE [dbo].[Secrets] CHECK CONSTRAINT [FK_Secrets_Users]
+GO
