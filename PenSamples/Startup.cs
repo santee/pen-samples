71b60b779664a92b7f346f3a5142906d5e4fddaf
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2015-02-24 15:44:42 +0300

    InitCommit

---
+++ PenSamples/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PenSamples.Startup))]
+namespace PenSamples
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
