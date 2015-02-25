44fdd356ac88d0bc9394d044d2fb5a7f92a2a24b
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2015-02-25 09:43:47 +0300

    Added middleware to handle new users

--- PenSamples.Web/Startup.cs
+++ PenSamples.Web/Startup.cs
@@ -6,17 +6,60 @@ using PenSamples.Web;
 
 namespace PenSamples.Web
 {
+    using System;
+    using System.Data;
+    using System.Data.Entity;
+    using System.Threading.Tasks;
     using System.Web.Mvc;
     using System.Web.Routing;
 
     using Owin;
 
+    using PenSamples.Web.Data;
+
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
+            app.Use(NewUserHandler);
+
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        private static async Task NewUserHandler(IOwinContext context, Func<Task> next)
+        {
+            var user = context.Authentication.User;
+            if (user != null)
+            {
+                var username = user.Identity.Name;
+
+                using (var entities = new PenEntities())
+                {
+                    using (var transaction = entities.Database.BeginTransaction(IsolationLevel.Serializable))
+                    {
+                        try
+                        {
+                            var userEntity = await entities.Users.FirstOrDefaultAsync(x => x.Name == username);
+                            if (userEntity == null)
+                            {
+                                entities.Users.Add(new User() { Name = username });
+
+                                await entities.SaveChangesAsync();
+                            }
+
+                            transaction.Commit();
+                        }
+                        catch
+                        {
+                            transaction.Rollback();
+                            throw;
+                        }
+                    }
+                }
+            }
+
+            await next.Invoke();
         }
     }
 }
