1b77437bda9b5c84e236697178b517073dd2f574
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2015-02-25 15:00:27 +0300

    Added bonus controller for JSON Hijacking

---
+++ PenSamples.Web/Controllers/BonusController.cs
@@ -0,0 +1,27 @@
+﻿namespace PenSamples.Web.Controllers
+{
+    using System.Dynamic;
+    using System.Web.Mvc;
+
+    public class BonusController : Controller
+    {
+        public JsonResult Get()
+        {
+            dynamic obj1 = new ExpandoObject();
+            obj1.password = "qwerty";
+
+            return new JsonResult() { Data = new[] { obj1 }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        public JsonResult GetAsObject()
+        {
+            dynamic obj1 = new ExpandoObject();
+            obj1.password = "qwerty";
+
+            dynamic wrapper = new ExpandoObject();
+            wrapper.d = new[] { obj1 };
+
+            return new JsonResult() { Data = wrapper, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+    }
+}
